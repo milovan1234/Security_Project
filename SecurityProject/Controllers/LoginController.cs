a133566659177141061e8d311b5f65c8545c2e63
Milovan Srejić <srejicmilovan@gmail.com>
2021-01-05 22:46:25 +0100

    Add sql injection problem

--- SecurityProject/Controllers/LoginController.cs
+++ SecurityProject/Controllers/LoginController.cs
@@ -1,21 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Data.SqlClient;
 using Microsoft.AspNetCore.Mvc;
+using SecurityProject.Models;
 
 namespace SecurityProject.Controllers
 {
     public class LoginController : Controller
     {
         public IActionResult Index()
-        {
+        {            
             return View();
         }
 
-        public IActionResult SqlInjection()
+        [HttpPost]
+        public IActionResult SqlInjection([FromBody] User user)
         {
-            return View();
+            SqlConnection connection = new SqlConnection(@"Server=(localdb)\MSSQLLocalDB;Database=SECURITY_DATABASE;Trusted_Connection=True;");
+            try
+            {
+                connection.Open();
+                SqlCommand sqlCommand = new SqlCommand("SELECT COUNT(*) FROM Users where Username='" + user.Username + "' AND Password='" + user.Password + "'", connection);
+                int login = (int)sqlCommand.ExecuteScalar();
+                if (login == 0)
+                {
+                    return BadRequest();
+                }                    
+                else
+                {
+                    return Ok();
+                }
+            }
+            catch
+            { }
+            finally
+            {
+                connection.Close();
+            }
+            return BadRequest();
         }
     }
 }
---
+++ SecurityProject/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecurityProject.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int NumberOfInvalidLogin { get; set; }
+        public DateTime LastInvalidLogin { get; set; }
+    }
+}
