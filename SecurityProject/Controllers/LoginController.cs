035779d278ac5cf7f976ef7dea70bfbfd67d15b4
Milovan Srejić <srejicmilovan@gmail.com>
2021-01-07 22:06:20 +0100

    Added Safe Sql Check

--- SecurityProject/Controllers/LoginController.cs
+++ SecurityProject/Controllers/LoginController.cs
@@ -41,20 +41,42 @@ namespace SecurityProject.Controllers
         [HttpPost]
         public IActionResult DictionaryAttack([FromBody] User user)
         {
-            SqlConnection connection = new SqlConnection(@"Server=DESKTOP-OVA2KPB\SQLEXPRESS;Database=SECURITY_DATABASE;Trusted_Connection=True;");
+            bool isUserFound = SafeSqlCheck(user);
+            if (isUserFound)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult SecureLogin([FromBody] User user)
+        {
+            bool isUserFound = SafeSqlCheck(user);
+            if (isUserFound)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        private bool SafeSqlCheck(User user)
+        {            
+            SqlConnection connection = new SqlConnection(@"Server=(localdb)\MSSQLLocalDB;Database=SECURITY_DATABASE;Trusted_Connection=True;");
             try
             {
                 connection.Open();
-                SqlCommand sqlCommand = new SqlCommand("SELECT COUNT(*) FROM Users where Username='" + user.Username + "' AND Password='" + user.Password + "'", connection);
-                int login = (int)sqlCommand.ExecuteScalar();
-                if (login == 0)
-                {
-                    return BadRequest();
-                }
-                else
-                {
-                    return Ok();
-                }
+                SqlCommand command = new SqlCommand("SELECT COUNT(*) FROM Users WHERE Username=@username AND Password=@password");
+                command.Parameters.AddWithValue("@username", user.Username);
+                command.Parameters.AddWithValue("@password", user.Password);
+                int login = (int)command.ExecuteScalar();
+                return login != 0;
             }
             catch
             { }
@@ -62,7 +84,7 @@ namespace SecurityProject.Controllers
             {
                 connection.Close();
             }
-            return BadRequest();
+            return false;
         }
     }
 }
